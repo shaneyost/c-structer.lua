c5d14898b89ae682e9b93e56cee513ffc7e51bab
Shane Yost <scy@shaneyost.io>
2025-05-03 16:16:48 -0600

    Update example, config and removed init mod

--- cstructer/init.lua
+++
@@ -1,4 +0,0 @@
-local M = {}
-M.structer = require("cstructer.structer")
-M.utils = require("cstructer.structer_utils")
-return M
--- example_luaapp.lua
+++ example_luaapp.lua
@@ -1,14 +1,17 @@
 #!/usr/bin/env luajit
-
 local tbl = require("cstructer.structer")
 local cfg = require("example_config")
 
-local Constructor, vals = tbl.create_struct(cfg)
-local Model = Constructor(vals)
-Model.fout.col = 8
-Model:table()
-print(Model.int8_v)
+local Constructor, Values = tbl.create_struct(cfg)
+local Model = Constructor(Values)
+
 print(Model)
+print(Model.int8_v)
+Model.int8_v = 10
+print(Model)
+print(Model.int8_v)
+
+Model:table()
 print("hash: ", Model:hash())
 print("file: ", Model.tbin)
 print("algo: ", Model.algo)
